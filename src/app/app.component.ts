819cfcf01747b32e029db5db712500a438307c8a
D. David Alvarez Rivero <d.david.ar10@gmail.com>
2021-12-09 13:35:02 -0400

    Integracion de plugin de impresion

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -16,6 +16,17 @@ export class AppComponent implements OnInit {
   ngOnInit(): void {
     this.printerService.getPrintersInstalled().subscribe(resp => {
       this.printerList = resp;
+    }, error => {
+      console.log(`error`, error);
+    });
+  }
+
+  onPrintDocument(printerName: string): void {
+    const docUrl = 'https://parzibyte.github.io/plugin-silent-pdf-print-examples/ticket.pdf';
+    this.printerService.getPrintDocument(docUrl, printerName).subscribe(resp => {
+      alert('Impresion completa!');
+    }, error => {
+      console.log(`error`, error);
     });
   }
 
--- src/app/shared/services/printer-plugin.service.ts
+++ src/app/shared/services/printer-plugin.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpParams } from '@angular/common/http';
 
 
 @Injectable({
@@ -12,6 +12,13 @@ export class PrinterPluginService {
 
   getPrintersInstalled(): Observable<string[]> {
     return this.http.get<string[]>(`http://localhost:8000/impresoras`);
+  }
+
+  getPrintDocument(urlFile: string, printerName: string): Observable<any> {
+    let params = new HttpParams();
+    params = params.append('urlPdf', urlFile);
+    params = params.append('impresora', printerName);
+    return this.http.get<any>(`http://localhost:8080/url`, { params });
   }
 
 }
